eac94acfd5918ed5b9ae348716e990ba557b7d70
Oscar Pacheco <Oscarpa88@yahoo.com>
2020-06-08 18:08:52 -0500

    Update ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,9 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
-    return None
+def get_indices_of_item_weights(weights, length, limit):  
+    hashTable = {}
+    
+    for i in range(length):
+        if limit - weights[i] in hashTable:
+            return [i, hashTable[limit - weights[i]]]
+        else:
+            hashTable[weights[i]] = i
+    print(hashTable)
